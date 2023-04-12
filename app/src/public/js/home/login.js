03d49962ab70c9386b290d3530eb3fe3d1a2a22e
chang <py1150566@gmail.com>
2023-04-12 23:04:34 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector('#id'),
+pw = document.querySelector('#pw'),
+loginBtn = document.querySelector('button');
+
+const login = () => {
+  const req ={
+    id : id.value,
+    pw : pw.value,
+  };
+  console.log(req);
+}
+
+loginBtn.addEventListener("click", login);
